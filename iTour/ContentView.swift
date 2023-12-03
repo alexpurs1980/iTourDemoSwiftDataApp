cb4c264f5d74f093d7d9f3e073c26158ce002b0f
Aleksey <alexpurs@yandex.ru>
2023-12-03 15:41:43 +0300

    Add @Query macro and tool for display info in ContentView

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -6,14 +6,25 @@
 //  
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    //Add our database. Macro @Query readout info from DB and than can display it
+    @Query var destinations: [Destination]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationStack {
+                List {
+                    ForEach(destinations) {destination in
+                        VStack(alignment: .leading) {
+                            Text(destination.name)
+                                .font(.headline)
+                            Text(destination.date.formatted(date: .long, time: .shortened))
+                        }
+                    }
+                }
+            }
         }
         .padding()
     }
